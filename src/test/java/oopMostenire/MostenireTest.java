c119c1ed2c3da4f19bd98daa20bfada5c829385d
MadalinaChera <mail.madalina.chera@gmail.com>
2025-11-10 19:23:00 +0200

    Mostenire/Incapsulare

--- src/test/java/oopMostenire/MostenireTest.java
+++ src/test/java/oopMostenire/MostenireTest.java
@@ -10,8 +10,17 @@ public class MostenireTest {
 
         Angajat angajat = new Angajat("Chera", "Madalina", 30, "Timisoara", "PanByViolett", 5000,"Nedeterminat", 21);
         angajat.infoAngajat();
-        System.out.println();
+
+        System.out.println("angajat.getVarsta();");
+        angajat.setVarsta(23);
+        System.out.println("angajat.getVarsta();");
+
         Sportiv sportiv = new Sportiv("Popi", "Balanel", 25, "Columbia", "Fotbal", true, 10);
         sportiv.infoSportiv();
+
+        System.out.println();
+
+        Student student = new Student("Cheza", "Bobita", 22, "Mangalia", "UPT", "Management", 2, false);
+        student.infoStudent();
     }
 }
